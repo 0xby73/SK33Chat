a54e39f82fc780d273c0b95f7e060e30342ee1f4
triptych <162903509+0xby73@users.noreply.github.com>
2024-11-06 01:34:56 +0000

    Update ascii.h

--- src/includes/ascii.h
+++ src/includes/ascii.h
@@ -7,16 +7,16 @@ namespace Ascii
 static void Header_Client() noexcept
 {
   std::cout << LIGHT_BLUE << R"(
-   __ _ _ __  _ __ ___  _ __
-  / _` | '_ \| '_ ` _ \| '_ \
- | (_| | | | | | | | | | |_) |
-  \__,_|_| |_|_| |_| |_| .__/
-                       | |
-                       |_|
+  ____  _  ____________  ____ _   _    _  _____
+ / ___|| |/ /___ /___ / / ___| | | |  / \|_   _|
+ \___ \| ' /  |_ \ |_ \| |   | |_| | / _ \ | |
+  ___) | . \ ___) |__) | |___|  _  |/ ___ \| |
+ |____/|_|\_\____/____/ \____|_| |_/_/   \_\_|
+
 )" << RESET;
 
   std::cout << "- Version 1.0\n";
-  std::cout << "- 2024 - triptych <0xby73>\n\n";
+  std::cout << "- 2024 - Triptych <0xby73>\n\n";
 
   std::cout << "- Type '!help' for the command list\n\n";
 }
