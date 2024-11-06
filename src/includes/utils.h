d117bce34e87cf47bc5a7138f713d942448bd558
triptych <162903509+0xby73@users.noreply.github.com>
2024-11-06 02:27:04 +0000

    Update utils.h

--- src/includes/utils.h
+++ src/includes/utils.h
@@ -9,5 +9,17 @@ namespace Utils
 		constexpr auto clear = "\033[2J\033[1;1H";
 		std::cout << clear << std::flush;
 	}
-}
 
+    const std::string Get_Time() noexcept
+    {
+        time_t     now = time(0);
+        struct tm  tstruct;
+        char       buf[80];
+        tstruct = *localtime(&now);
+
+        strftime(buf, sizeof(buf), "[ %X ]", &tstruct);
+
+        return buf;
+    }
+
+}
